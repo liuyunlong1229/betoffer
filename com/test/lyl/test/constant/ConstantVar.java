e6cf84c99a728604749f68f6c2b0282b0182f298
liuyunlong <Lyl&%#2020>
2025-01-06 14:58:06 +0800

    :hammer: 定义公共的常量类

---
+++ com/test/lyl/test/constant/ConstantVar.java
@@ -0,0 +1,16 @@
+package com.test.lyl.test.constant;
+
+
+/**
+ * 工程常量定义类
+ */
+public class ConstantVar {
+
+
+    /**
+     * token过期时间：10分钟
+     */
+    public static final int TOKEN_EXPIRED_TIME=10 * 60 * 1000;
+
+    public static final String RANDOM_STRING_FACTORY="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+}
